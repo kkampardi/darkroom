234b9baed46f1e43cc0410e08199f66fec64f972
pycat <kkampardi@gmail.com>
2017-04-11 11:57:03 +0300

    Media conf #4
    - Add configuration for statics and media root

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -122,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL.strip("/"))
+MEDIA_URL = STATIC_URL + "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, *MEDIA_URL.strip("/").split("/"))
