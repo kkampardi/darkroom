be31c8218b21327ade22849c123563749f8ba08b
pycat <kkampardi@gmail.com>
2017-04-28 13:09:29 +0300

    Add User tests #7

--- user/models.py
+++ user/models.py
@@ -1,14 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.conf import settings
-
 # Create your models here.
 
 
 # extend the user model
 class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
-    nickname = models.CharField(max_length=200, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    nickname = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return 'Profile for user {}'.format(self.user.username)
 
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Profile
+
+# Model test suit
+class ModelTestCase(TestCase):
+
+    def setUp(self):
+        self.nick_name = "rockstar"
+        self.profile = Profile(user="nick_name")
+
+    def test_model_can_create_a_user(self):
+        objects_count = Profile.objects.count()
+        self.profile.save()
+        objects_new_count = Profile.objects.count()
+        self.assertNotEqual(objects_count, objects_new_count)
